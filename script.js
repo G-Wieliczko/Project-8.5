052759dbcceb0b692a2b1c1e4196ed3974b8c8c6
Grzegor Wieliczko <gw851015@gmail.com>
2017-07-04 11:08:05 +0200

    fixed details

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var a = 5
-	h = 8
-	triangleArea = a * h / 2
+var a = 5,
+	h = 8,
+	triangleArea = a * h / 2;
 
 	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
